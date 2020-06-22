72409f8ba68992e26594a0959efddd9285c5491f
Ning An <ning.an@outlook.com>
2020-06-22 11:59:14 -0400

    Exercise1-solution

--- workshop/exercises-P1_theory/exercise-1.js
+++ workshop/exercises-P1_theory/exercise-1.js
@@ -19,14 +19,34 @@ const reverseString = (str) => {
 };
 
 // Expand each of the following and get the result of the expression
+// What will happen in each step as the function is called?
+// Write out your answer comments.
+
 // 1-1
 square(decrement(square(decrement(3))));
+
+//First decrement(3) will be called and return 2;
+// then square(2) will be called and return 4;
+// then decrement(4) will be called and return 3;
+// finally square(3) will be called and return 9.
 
 // 1-2
 decrement(decrement(square(square(3))));
 
+// First square(3) will be called and return 9;
+// then square(9) will be called and return 81;
+// then decrement(81) will be called and return 80;
+// finally decrement(80) will be called and return 79.
+
 // 1-3
 duplicateString(reverseString('hello'));
 
+// First reverseString('hello') will be called and return 'olleh';
+// then duplicateString('olleh') will be called and return 'olleholleh'.
+
 // 1-4
 reverseString(duplicateString(duplicateString('foo')));
+
+// First duplicateString('foo') will be called and return 'foofoo';
+// then duplicateString('foofoo') will be called and return 'foofoofoofoo';
+// finally reverseString('foofoofoofoo') will be called and return 'oofoofoofoof'.
