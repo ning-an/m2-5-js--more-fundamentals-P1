da05749ac642594f23effd06af3c7c64f0be8829
Ning An <ning.an@outlook.com>
2020-06-22 15:54:14 -0400

    Exercise2.7-solution

--- workshop/exercises-P2_tests/exercise-2-7.js
+++ workshop/exercises-P2_tests/exercise-2-7.js
@@ -13,10 +13,28 @@
 
 function repeat(arr) {
   // Your code here
+  if (typeof arr[0] !== 'string' || typeof arr[1] !== 'number') {
+    return undefined;
+  } else if (arr[1] <= 0) {
+    return '';
+  } else {
+    let res = '';
+    for (let i = 0; i < arr[1]; i++) {
+      res += arr[0];
+    }
+    return res;
+  }
 }
 
 // We need 7 test cases.
 // Don't forget to test all of the question parameters
+expect(repeat(['foo', 0]), '');
+expect(repeat(['foo', -3]), '');
+expect(repeat([21, 32]), undefined);
+expect(repeat(['foo', 'foo']), undefined);
+expect(repeat([21, 'foo']), undefined);
+expect(repeat(['foo', 2]), 'foofoo');
+expect(repeat(['foo', 5]), 'foofoofoofoofoo');
 
 /**
  * -------------------------------------------------------------------
