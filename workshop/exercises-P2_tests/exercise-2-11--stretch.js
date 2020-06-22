1ce282862951a083294c0ea51caf38cc4324cde3
Ning An <ning.an@outlook.com>
2020-06-22 16:49:43 -0400

    Exercise2.11-solution

--- workshop/exercises-P2_tests/exercise-2-11--stretch.js
+++ workshop/exercises-P2_tests/exercise-2-11--stretch.js
@@ -8,11 +8,28 @@
 
 function addNumbers(...nums) {
   // Insert missing solution please
+  let sum = 0;
+  if (nums.length === 0) {
+    return undefined;
+  } else {
+    nums.forEach(num => {
+      if (typeof num === 'number') {
+        sum += num ** 2;
+      }
+    })
+  }
+  return sum;
 }
 
 // Add 7 more test cases.
 expect(addNumbers(1, 2, 3), 14); // 1 + 2**2 + 3**2 = 1 + 4 + 9 = 14
-
+expect(addNumbers(1, 'something', '2'), 1);
+expect(addNumbers(1, 'sometthing', 4), 17);
+expect(addNumbers(), undefined);
+expect(addNumbers(2, 3, 4, 5), 54);
+expect(addNumbers(1, 1, 1, 1, 1), 5);
+expect(addNumbers(1, 3, 'something', true), 10);
+expect(addNumbers(3, 3, 3), 27);
 /**
  * -------------------------------------------------------------------
  * ⚠️ No changes necessary below. ⚠️
