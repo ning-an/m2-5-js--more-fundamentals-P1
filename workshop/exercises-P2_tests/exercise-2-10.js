0655289410aad9bfd1197269f2fa991f7e927ebd
Ning An <ning.an@outlook.com>
2020-06-22 16:41:27 -0400

    Exercise2.10-solution

--- workshop/exercises-P2_tests/exercise-2-10.js
+++ workshop/exercises-P2_tests/exercise-2-10.js
@@ -10,9 +10,16 @@
 //  - Split the input sentence into an array of words
 //  - Iterate over each word, calling your "capitalize word" function
 //  - Join the results into a string
+function capitalizeWord(word) {
+  return word[0].toUpperCase() + word.slice(1);
+}
 
 function makeIntoTitle(sentence) {
   // Your code here
+  let array = sentence.split(' ');
+  let newArray = array.map(elem => capitalizeWord(elem));
+  let res = newArray.join(' ');
+  return res;
 }
 
 // Add 6 total (5 more)
@@ -20,6 +27,11 @@ expect(
   makeIntoTitle('the longest road is a great song'),
   'The Longest Road Is A Great Song'
 );
+expect(makeIntoTitle('hello world'), 'Hello World');
+expect(makeIntoTitle('handle events book'), 'Handle Events Book');
+expect(makeIntoTitle('career service'), 'Career Service');
+expect(makeIntoTitle('upcoming events are posted at the following website'), 'Upcoming Events Are Posted At The Following Website');
+expect(makeIntoTitle('the national'), 'The National');
 
 /**
  * -------------------------------------------------------------------
