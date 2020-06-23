be1ed4252770e7f8cb2e8b7f38f25e8ca7b51d68
Ning An <ning.an@outlook.com>
2020-06-22 22:02:09 -0400

    Excercise3.1-solution

--- workshop/exercises-P3_problems/exercise-3-1/app.js
+++ workshop/exercises-P3_problems/exercise-3-1/app.js
@@ -6,3 +6,26 @@
 // Guidelines
 // - write only one event listener
 // - use a switch statement
+const btns = document.querySelector('.buttons');
+
+function clickHandler(e) {
+    e.preventDefault();
+    let btnId = e.target.id;
+    let btn = document.querySelector(`#${btnId}`);
+    switch (btnId) {
+        case 'btn-1':
+            btn.style.opacity = 0;
+            break;
+        case 'btn-2':
+            btn.style.backgroundColor = 'crimson';
+            break;
+        case 'btn-3':
+            btn.style.backgroundColor = 'lightblue';
+            break;
+        case 'btn-4':
+            btn.classList.add('jitters');
+            break;
+    }
+}
+
+btns.addEventListener('click', clickHandler);
