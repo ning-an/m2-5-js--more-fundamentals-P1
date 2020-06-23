793726d1e11256b73c4a0bf643f2c149cdc6afe8
Ning An <ning.an@outlook.com>
2020-06-23 16:40:15 -0400

    Exercise3.5-solution

--- workshop/exercises-P3_problems/exercise-3-5--stretch/app.js
+++ workshop/exercises-P3_problems/exercise-3-5--stretch/app.js
@@ -5,3 +5,24 @@
 // - use the 'blur' event
 // - one event listener per input
 // - If you need extra hints, see somebody... :)
+const board = document.querySelector('.board');
+board.style.width = '600px';
+board.style.height = '600px';
+
+const colInput = document.querySelector('.columns-input');
+const rowInput = document.querySelector('.rows-input');
+
+function createGrid() {
+    let cols = colInput.value;
+    let rows = rowInput.value;
+    board.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    for (let i = 0; i < cols * rows; i++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        board.appendChild(cell);
+    }
+}
+colInput.addEventListener('change', createGrid);
+rowInput.addEventListener('change', createGrid);
+
