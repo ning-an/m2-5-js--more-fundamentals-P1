a23fc05ace072018bc4338d67bc96b1fa315295b
Ning An <ning.an@outlook.com>
2020-06-23 09:19:16 -0400

    Exercise3.4-solution

--- workshop/exercises-P3_problems/exercise-3-4/app.js
+++ workshop/exercises-P3_problems/exercise-3-4/app.js
@@ -1,2 +1,17 @@
 // Exercise 3-4 - The GRID
 // ------------
+let col = 10;
+let row = 10;
+
+const board = document.querySelector('.board');
+board.style.width = '600px';
+board.style.height = '600px';
+
+board.style.gridTemplateRows = `repeat(${row}, 1fr)`;
+board.style.gridTemplateColumns = `repeat(${col}, 1fr)`;
+
+for (let i = 0; i < col * row; i++) {
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+    board.appendChild(cell);
+}
